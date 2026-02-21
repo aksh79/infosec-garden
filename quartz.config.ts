055707752694810dd16e92c7f90ff838baf11e2a
akshay <akshay@do-no-reply.com>
2026-02-20 22:48:16 -0500

    title update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "aksh79's infosec garden",
+    pageTitle: "aksh79",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
